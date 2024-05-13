1cb05a7ac64d88d50fcb0fcf70c536b29d648c0e
mellieator <christopherbarry393@gmail.com>
2024-05-12 22:34:53 -0700

    Final *FINAL* version. Removed copy of main.py and cleaned up code.

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
 def main():
-
     path_to_file = "books/frankenstein.txt"
+    contents = open_file(path_to_file)
+    count_of_words = word_count(contents)
+    count_of_characters = letter_count(contents)
+    sorted_characters = sorted_character_count(count_of_characters)
+    #print(contents)
+    #print(count_of_words)
+    #print(count_of_characters)
 
-    with open(path_to_file, 'r') as file:
-        contents = file.read()
-        return contents
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_of_words} words were found in the document.")
+    print()
 
+    for item in sorted_characters:
+        key = item['key']
+        value = item['value']
+        print(f"The '{key}' character was found {value} times.")
     
+    print()
+    print("--- End report ---")
+    
+def open_file(path_to_file):
+    with open(path_to_file, 'r') as file:
+        file_text = file.read()
+    return file_text
+
 def word_count(contents):
     word_list = contents.split()
-
-    count_of_words = len(word_list)
-
-    return count_of_words
+    words = len(word_list)
+    return words
 
 def letter_count(contents):
-    lower_contents = contents.lower()
-    return lower_contents
+    letter_dict = {}
+
+    for char in contents:
+        if char.isalpha():
+            char = char.lower()
+            letter_dict[char] = letter_dict.get(char, 0) +1
+
+    return letter_dict
+
+def sorted_character_count(count_of_characters):
+    dictionary_list = []
+
+    for key, value in count_of_characters.items():
+        new_dict = {'key': key, 'value': value}
+        dictionary_list.append(new_dict)
+
+    def sort_on(item):
+        return item['value']
+
+    dictionary_list.sort(reverse=True, key=sort_on)
+
+    return dictionary_list
 
 if __name__ == "__main__":
-    file_contents = main()
-    count_of_words = word_count(file_contents)
-    print(lower_contents)
+
+    main()
